7378198f976162bc233628d2f0e9fde47ec256e7
shanthan <shanthanreddyvangala@gmail.com>
2020-01-02 10:59:36 +0530

    first checkin

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express() ('path')
-const port = app.use(express.static (_dirname + '/distngproject'));
+const path=required('path')
+const app = express()
+const port = process.env.PORT||3000
+ app.use(express.static (_dirname + '/distngproject'));
 
 app.get('/', (req, res) => res.sendFile(path.join(_dirname + '/dist/ngproject/index.html')));
 
